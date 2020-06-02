d99aa4e817d95bed4a8eabcc9f4f2f53db157cb0
Lng123 <35930836+Lng123@users.noreply.github.com>
2020-06-02 16:10:50 -0700

    Updates

---
+++ precache-manifest.b8bc4f798ce4a987287afe756508e506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ff05ffd6489162df32cb7a9ca713c5",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "c3efcf96c5f29b344e3b",
+    "url": "/MyPortfolio/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "c9366eb6d089f2c126be",
+    "url": "/MyPortfolio/static/js/2.26d0daa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyPortfolio/static/js/2.26d0daa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3efcf96c5f29b344e3b",
+    "url": "/MyPortfolio/static/js/main.f850703d.chunk.js"
+  },
+  {
+    "revision": "59918b864e4e41672676",
+    "url": "/MyPortfolio/static/js/runtime-main.7bb13974.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MyPortfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);

1c8267119b1cafe3a2f70fd3f55c9e98d256b89e
Lng123 <35930836+Lng123@users.noreply.github.com>
2020-06-02 17:24:05 -0700

    title and index modules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Projects from './components/Projects'
 import * as serviceWorker from './serviceWorker';
+import './index.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component{
+  render() {
+      return(
+          <div>
+              {/* <Navigation/> */}
+              <Header/>
+              <Projects/>
+          </div>
+      );
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

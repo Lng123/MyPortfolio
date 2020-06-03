985648cbb31f30677f3fe232a44ecb0aa6a5fc82
Lng123 <35930836+Lng123@users.noreply.github.com>
2020-06-02 17:39:15 -0700

    Header css and Project css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Projects from './components/Projects'
 import * as serviceWorker from './serviceWorker';
